e5259720643efc10fbaf5cde0f4bc13f2024b8ce
Ms-patel <patelmilind1620@gmail.com>
2022-02-02 17:41:41 +0530

    models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from app.models import Coat, Frame, Glass, Ilpatti, Length, Mnet, Product, Quotation, Shutter, Uchannel
+
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Frame)
+admin.site.register(Length)
+admin.site.register(Coat)
+admin.site.register(Shutter)
+admin.site.register(Ilpatti)
+admin.site.register(Uchannel)
+admin.site.register(Glass)
+admin.site.register(Mnet)
+admin.site.register(Quotation)
--- app/models.py
+++ app/models.py
@@ -3,9 +3,59 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+    name = models.CharField(verbose_name="Product Name",max_length=50)
 
-    name = models.CharField(verbose_name="Product Name",max_length=30)
-    
+    def __str__(self):
+        return self.name
+
+class Frame(models.Model):
+    name = models.CharField(verbose_name="Frame Name",max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Length(models.Model):
+    name = models.CharField(verbose_name="Length",max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Coat(models.Model):
+    name = models.CharField(verbose_name="Coating Name",max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Shutter(models.Model):
+    name = models.CharField(verbose_name="Shutter Name",max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Ilpatti(models.Model):
+    name = models.CharField(verbose_name="I/l Patti Name",max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Uchannel(models.Model):
+    name = models.CharField(verbose_name="Uchannel Name",max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Glass(models.Model):
+    name = models.CharField(verbose_name="Glass Name",max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Mnet(models.Model):
+    name = models.CharField(verbose_name="Mosquito Net Name",max_length=50)
+
+    def __str__(self):
+        return self.name
+
 
 class Quotation(models.Model):
 
